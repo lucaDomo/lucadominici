9bfb82a9ac6391e38897c373e76fe98b718b2c55
Luca Dominici <domoluca00@gmail.com>
2024-02-01 17:35:33 +0100

    update content

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
 import { FormazioneComponent } from '../formazione/formazione.component';
+import { SkillComponent } from '../skill/skill.component';
+import { WorkExperienceComponent } from '../work-experience/work-experience.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [AboutComponent, FormazioneComponent],
+  imports: [AboutComponent, FormazioneComponent, SkillComponent, WorkExperienceComponent, ContactComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  close(){
+    var el = document.getElementById("navbar")
+    if(el!=null){
+      el.style.display = "none"
+      el = <HTMLDivElement>document.getElementById("x_btn")
+      el.style.display = "none"
+      el = <HTMLDivElement>document.getElementById("hamburger")
+      el.style.display = "block"
+    }
+  }
+
+  open(){
+    var el = document.getElementById("navbar")
+    if(el!=null){
+      el.style.display = "block"
+      el = <HTMLDivElement>document.getElementById("x_btn")
+      el.style.display = "block"
+      el = <HTMLDivElement>document.getElementById("hamburger")
+      el.style.display = "none"
+    }
+  }
 
 }
